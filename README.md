b1ba67d9a898085d5ffa8b09dadf0f66363266da
Irfan Durmus <irfandurmus@gmail.com>
2010-11-07 23:44:59 +0200

    moved a new repository

---
+++ README.md
@@ -0,0 +1,11 @@
+# jQuery Periodic Updater
+[jQuery Periodic Updater](http://irfandurmus.com/projects/jquery-periodic-updater) is a [jQuery](http://jquery.com) plugin.
+
+## Author
+Copyright 2010, Irfan Durmus
+
+## License
+Dual licensed under the MIT or GPL Version 2 licenses.
+
+
+[Please visit plugin page to demo, how to use and change logs](http://irfandurmus.com/projects/jquery-periodic-updater)
---
+++ updater-v0.2-beta.js
@@ -0,0 +1,152 @@
+/* jQuery Periodic Updater
+ * 
+ * @Author
+ * Copyright Nov 03 2010, Irfan Durmus - http://irfandurmus.com/
+ *
+ * @Version
+ * 0.2 Beta
+ *
+ * @License
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * See the README file or visit the plugin page.
+ * http://irfandurmus.com/projects/jquery-periodic-updater
+ *
+ */
+
+;(function($){
+	// global variables
+	doc = $(document);
+	
+	$.fn.updater = function(userOpts, callback){
+		
+		// extend options
+		var opts = $.extend(true, {}, defaults, userOpts),
+			interval = setInterval(actions.init, opts.interval);
+		
+		// write options, callback and interval method into jquery data
+		doc.data('updater', {
+			options : opts,			// save the options 
+			callback : callback,	// save the callback function
+			interval : interval,	// save the interval to data
+			isActive : true,		// avoid double timer,
+			lastRequest: {
+				result: '',
+				status: '',
+				text: ''
+			}
+		});
+		
+		// initialize the plugin
+		actions.init();
+		
+		doc.bind('stop.updater', actions.stop);
+		doc.bind('start.updater', actions.reboot);
+		return;
+	};
+	
+	// action list
+	var actions = {
+		init: function(){
+			
+			// if timer true, start the pull
+			if (actions.obj().isActive) {
+				// get options and callback
+				var opts = actions.obj().options,
+					callback = actions.obj().callback;
+				
+				// call pull method
+				actions.pull(opts, callback);
+			};
+			
+			return;
+		},
+		
+		setStatus: function(response, result){
+			actions.obj().lastRequest.result = result;
+			actions.obj().lastRequest.status = response.status;
+			actions.obj().lastRequest.text = response.statusText;
+			return;
+		},
+		
+		pull:function(opts, callback){
+			// set ajax method
+			var method = (opts.method === 'post') ? 'POST' : 'GET';
+			
+			$.ajax({
+				url : opts.url,
+				data: opts.data,
+				type: method,
+				dataType: opts.response,
+				success: function(obj, result, response){
+					
+					// set last request response with success
+					actions.setStatus(response, result);
+					
+					// call the callback method
+					callback(obj, result, response);
+				},
+				error: function(response, result){
+					
+					// set last request response with error
+					actions.setStatus(response, result);
+				}
+			})
+			return;
+		},
+		
+		stop: function(){
+			
+			// if timer true clear the timer and set isActive value false
+			if (actions.obj().isActive) {
+				clearInterval(actions.obj().interval);
+			};
+			actions.obj().isActive = false;
+			return;
+		},
+		
+		reboot:function(){
+			// if timer false re-initialize the plugin and set isActive value true
+			if (!actions.obj().isActive) {
+				var opts = actions.obj().options,
+					cb = actions.obj().callback;
+				$.updater(opts, cb);
+			};
+			actions.obj().isActive = true;
+			return;
+		},
+		
+		obj: function(){
+			return doc.data('updater');
+		},
+		debug: function(){
+			if (console) {
+				console.group();
+				console.log('Url: ', actions.obj().options.url);
+				console.log('Interval: ', actions.obj().options.interval);
+				console.log('Method: ', actions.obj().options.method);
+				console.log('Data: ', actions.obj().options.data);
+				console.log('Callback: ', actions.obj().callback);
+				console.log('Last Request: ', actions.obj().lastRequest.result, actions.obj().lastRequest.status, actions.obj().lastRequest.text);
+				console.log('Requests Continues: ', actions.obj().isActive);
+				console.groupEnd();
+			};
+			return;
+		}
+	}
+	
+	var defaults = {
+		url: undefined,
+		data: undefined,
+		method: 'post',
+		response: 'json',
+		interval: 3000,
+	};
+	
+	// alias for short usage
+	$.updater = $.fn.updater;
+	$.updater.stop = actions.stop;
+	$.updater.start = actions.reboot;
+	$.updater.debug = actions.debug;
+	
+})(jQuery);
